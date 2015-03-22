e5c22d40be43d66f466d38e459a0e251636c7ffe
Lilie Kho <liekho@gmail.com>
2015-03-22 23:08:18 +0800

    commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 ## > source('cachematrix.R')
 ## > m <- makeCacheMatrix(matrix(c(2, 1, 0, 2), c(2, 2)))
 ## > cacheSolve(m)
@@ -34,9 +29,6 @@ makeCacheMatrix <- function(x = matrix()) {
     getinverse = getinverse
   )
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
